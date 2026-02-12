87d7721a3d658033f4c3e17b99169e7cad756651
NH-Santos <natanaelsantospcg@gmail.com>
2026-02-12 13:15:33 -0300

    Update usuario.js

--- docs/usuario.js
+++ docs/usuario.js
@@ -1,1 +1,32 @@
+async function carregarUsuarioSeLogado() {
+    const token = localStorage.getItem("token");
 
+    if (!token) {
+        return; // Apenas não faz nada
+    }
+
+    try {
+        const response = await fetch("/auth/me", {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok) {
+            return;
+        }
+
+        const usuario = await response.json();
+
+        document.getElementById("saudacaoUsuario").innerText =
+            `Olá, ${usuario.login}`;
+
+        document.getElementById("avatarInicial").innerText =
+            usuario.login.charAt(0).toUpperCase();
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+carregarUsuarioSeLogado();
