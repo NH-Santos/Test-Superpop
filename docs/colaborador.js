9248a771b3397c68985aa8a8d6d75d860cf33be2
NH-Santos <natanaelsantospcg@gmail.com>
2026-02-18 16:51:48 -0300

    Remove log statement for added collaborator
    Remove console log for added collaborator

--- docs/colaborador.js
+++ docs/colaborador.js
@@ -45,7 +45,6 @@ document.addEventListener("DOMContentLoaded", () => {
                     });
 
                     lista.appendChild(li);
-                    console.log("Adicionado:", li)
                 });
 
             } catch (error) {
