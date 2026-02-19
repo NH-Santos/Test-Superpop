acfd667df774e05086711a10aa9b0fa6889370da
NH-Santos <natanaelsantospcg@gmail.com>
2026-02-19 11:37:07 -0300

    Update colaborador.js

--- docs/colaborador.js
+++ docs/colaborador.js
@@ -40,7 +40,7 @@ edocument.addEventListener("DOMContentLoaded", () => {
                     li.textContent = `${col.nome_completo} - ${col.setor}`;
 
                     li.addEventListener("click", () => {
-                        input.value = `${col.nome_completo}- ${col.setor}`;
+                        input.value = `${col.nome_completo} - ${col.setor}`;
                         lista.innerHTML = "";
                     });
 
